b4991ae5462ff92e415db703b7b420e2f9ef7439
Semmy <semmywong@126.com>
2024-12-27 15:56:20 +0800

    build: change build method

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,7 +2,7 @@
  * @Author: Semmy Wong
  * @Date: 2024-11-27 21:52:42
  * @LastEditors: Semmy Wong
- * @LastEditTime: 2024-11-27 22:01:05
+ * @LastEditTime: 2024-12-25 22:55:14
  * @Description: Description
  */
 import type { NextConfig } from "next";
@@ -10,7 +10,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   transpilePackages: ["@next-cms/ui"],
-  output: "standalone",
   experimental: {
   },
 };
